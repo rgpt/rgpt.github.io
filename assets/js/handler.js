1c76f4700b51677facb0302bcfe38d592bae22ca
Rishi <rishigpt2009@gmail.com>
2018-05-23 00:36:52 +0530

    Added skills and qualifications tab, added images for the skills tab and used Progress bar plugin for progress bar

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -1,5 +1,18 @@
-// $("#navBarTop li").on("click", function() {
-//       $("#navBarTop li").removeClass("active");
-//       $(this).addClass("active");
-//       $('#mainBodyContent').load("Demo.html")
-//     });
+var config = {
+  color: '#3b434a',
+  strokeWidth: 4,
+}
+var circle1 = new ProgressBar.Circle('#skill1', config);
+var circle2 = new ProgressBar.Circle('#skill2', config);
+var circle3 = new ProgressBar.Circle('#skill3', config);
+var circle4 = new ProgressBar.Circle('#skill4', config);
+var circle5 = new ProgressBar.Circle('#skill5', config);
+var circle6 = new ProgressBar.Circle('#skill6', config);
+var circle7 = new ProgressBar.Circle('#skill7', config);
+circle1.animate(0.9);
+circle2.animate(0.8);
+circle3.animate(0.7);
+circle4.animate(0.7);
+circle5.animate(0.9);
+circle6.animate(0.8);
+circle7.animate(0.6);
